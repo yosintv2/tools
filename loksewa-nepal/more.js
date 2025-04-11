ec2431379fbb13df8aeacf2c2381361699d39ed0
Anynomous <156112254+yosintv2@users.noreply.github.com>
2025-04-11 19:18:48 +0900

    Update more.js

--- loksewa-nepal/more.js
+++ loksewa-nepal/more.js
@@ -4,13 +4,13 @@ const pages = [
   { title: "Lok Sewa Set 2 Quiz", link: "set2.html" },
   { title: "Lok Sewa Set 3 Quiz", link: "set3.html" },
   { title: "Lok Sewa Set 4 Quiz", link: "set4.html" },
-  { title: "Lok Sewa Set 5 Quiz", link: "/loksewa-nepal/set5.html" },
-  { title: "Lok Sewa Set 6 Quiz", link: "/loksewa-nepal/set6.html" },
-  { title: "Lok Sewa Set 7 Quiz", link: "/loksewa-nepal/set7.html" },
-  { title: "Lok Sewa Set 8 Quiz", link: "/loksewa-nepal/set8.html" },
-  { title: "Lok Sewa Set 9 Quiz", link: "/loksewa-nepal/set9.html" },
-  { title: "Lok Sewa Set 10 Quiz", link: "/loksewa-nepal/set10.html" },
-  { title: "Lok Sewa Set 11 Quiz", link: "/loksewa-nepal/set11.html" }
+  { title: "Lok Sewa Set 5 Quiz", link: "set5.html" },
+  { title: "Lok Sewa Set 6 Quiz", link: "set6.html" },
+  { title: "Lok Sewa Set 7 Quiz", link: "set7.html" },
+  { title: "Lok Sewa Set 8 Quiz", link: "set8.html" },
+  { title: "Lok Sewa Set 9 Quiz", link: "set9.html" },
+  { title: "Lok Sewa Set 10 Quiz", link: "set10.html" },
+  { title: "Lok Sewa Set 11 Quiz", link: "set11.html" }
     ];
 
     const otherPages = pages.filter(p => !currentPage.includes(p.link));
